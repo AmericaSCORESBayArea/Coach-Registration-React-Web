04fe42f5174756196ecaa6a0082ad8d5eab6d7bf
Lautaro Rozenblum <lautirozen@gmail.com>
2023-04-01 18:47:02 -0300

    added log out and success modals

---
+++ src/components/utils/Modal.js
@@ -0,0 +1,55 @@
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
+function ModalwithConfirmation(
+  translations,
+  confirmFunction,
+  iconType,
+  dismissFunction
+) {
+  return Swal.fire({
+    title: translations.modal_title,
+    text: translations.modal_text,
+    icon: iconType,
+    showCancelButton: true,
+    reverseButtons: true,
+    confirmButtonColor: "#1976d2",
+    cancelButtonColor: "#616161",
+    footer: translations.modal_footer,
+    confirmButtonText: translations.modal_confirm_button,
+    cancelButtonText: translations.modal_cancel_button,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      confirmFunction();
+    } else if (result.dismiss === Swal.DismissReason.cancel) {
+      dismissFunction();
+    }
+  });
+}
+
+export { ModalwithConfirmation };
+
+function ErrorModal(translations, iconType) {
+  return Swal.fire({
+    title: translations.modal_title,
+    html: translations.modal_text,
+    icon: iconType,
+    confirmButtonColor: "#1976d2",
+    confirmButtonText: translations.modal_close_button,
+  });
+}
+export { ErrorModal };
+
+function SuccessModal(translations, iconType, dismissFunction) {
+  return Swal.fire({
+    title: translations.modal_title,
+    html: translations.modal_text,
+    icon: iconType,
+    confirmButtonColor: "#1976d2",
+    confirmButtonText: translations.modal_confirm_button,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      dismissFunction();
+    }
+  });
+}
+export { SuccessModal };
