96c81bcef66f0f34c04dece95efb2c18583732b0
Lautaro Rozenblum <lautirozen@gmail.com>
2023-04-02 12:34:55 -0300

    updated browserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Route from "./Routes";
 
 const baseTheme = createTheme();
@@ -10,13 +10,13 @@ if (`${process.env.REACT_APP_NODE_ENV}` === "production") {
   console.warn = () => {};
 }
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <ThemeProvider theme={baseTheme}>
       <div className="App">
         <Route />
       </div>
     </ThemeProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
