4940a52d56c1e15b845504d9e573de07f0563aa3
Lautaro Rozenblum <lautirozen@gmail.com>
2023-04-01 22:01:46 -0300

    fixed branch

---
+++ src/components/utils/PagesTitle.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PagesTitle() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <h1>Coach Registration</h1>
+    </div>
+  );
+}
+
+export { PagesTitle };
