007883b14457c331e7b166655461ddd80d7df910
Lautaro Rozenblum <lautirozen@gmail.com>
2023-04-26 19:50:49 -0300

    api connection

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,29 +1,67 @@
 import { useState, useEffect } from "react";
 import { Widget } from "@typeform/embed-react";
 import { SuccessModal } from "../utils/Modal";
+import Loading from "../utils/Loading";
 
 export default function HomeScreen() {
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight - 300);
+  const [loading, setLoading] = useState(false);
   const form_id = `${process.env.REACT_APP_TYPEFORM_FORM_ID}`;
   const updateDimensions = () => {
     setWidth(window.innerWidth);
     setHeight(window.innerHeight - 300);
   };
   const confirmedRegistration = () => {
-    window.top.location.href = 'https://scoresu.org/coach';
+    window.top.location.href = "https://scoresu.org/coach";
     //window.parent.location.open("https://scoresu.org/coach", "_self");
   };
-  const handleSubmit = (response) => {
-    const register_modal_success = {
-      modal_title: "Successful registration",
-      modal_text: `You have been successfully registered.<br /><b style="font-size:17px;" align="center">Download our app to start taking attendance.<br /><br /><a target="_parent" href="https://apps.apple.com/us/app/america-scores-attendance/id1527435979"><img src="https://iili.io/HOqRSRf.png" width="180" 
-      height="70" /></a> <a target="_parent" href="https://play.google.com/store/apps/details?id=com.americaScoresAttendance.app&hl=es_AR&gl=US"><img src="https://iili.io/HOq59WB.png" width="180"
-      height="70" /></a></b>`,
+  const confirmedError = () => {
+    window.top.location.href = "https://scoresu.org/coach/coach-registration";
+    //window.parent.location.open("https://scoresu.org/coach", "_self");
+  };
+  const handleSubmit = async (response) => {
+    setLoading(true);
+    const id = `${process.env.REACT_APP_CLIENT_ID}`;
+    const secret = `${process.env.REACT_APP_CLIENT_SECRET}`;
+    const form_id = `${process.env.REACT_APP_TYPEFORM_FORM_ID}`;
+    var myHeaders = new Headers();
+    myHeaders.append("client_id", id);
+    myHeaders.append("client_secret", secret);
+    myHeaders.append("Content-Type", "application/json");
 
-      modal_confirm_button: "DONE",
+    var requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
     };
-    SuccessModal(register_modal_success, "success", confirmedRegistration);
+
+    await fetch(
+      `https://salesforce-data-api-proxy-prod.us-e2.cloudhub.io/api/typeform/responses?formId=${form_id}&responseId=${response.responseId}`,
+      requestOptions
+    )
+      .then(() => {
+        setLoading(false);
+        const register_modal_success = {
+          modal_title: "Successful registration",
+          modal_text: `You have been successfully registered.<br /><b style="font-size:17px;" align="center">Download our app to start taking attendance.<br /><br /><a target="_parent" href="https://apps.apple.com/us/app/america-scores-attendance/id1527435979"><img src="https://iili.io/HOqRSRf.png" width="180" 
+        height="70" /></a> <a target="_parent" href="https://play.google.com/store/apps/details?id=com.americaScoresAttendance.app&hl=es_AR&gl=US"><img src="https://iili.io/HOq59WB.png" width="180"
+        height="70" /></a></b>`,
+
+          modal_confirm_button: "DONE",
+        };
+        SuccessModal(register_modal_success, "success", confirmedRegistration);
+      })
+      .catch((error) => {
+        setLoading(false);
+        const error_modal = {
+          modal_title: "Server error [500]",
+          modal_text: `There is an issue with our server. If this persists, please contact us.`,
+          modal_confirm_button: "DONE",
+        };
+        SuccessModal(error_modal, "error", confirmedError);
+        console.log("error", error);
+      });
   };
   useEffect(() => {
     window.addEventListener("resize", updateDimensions);
@@ -31,13 +69,17 @@ export default function HomeScreen() {
   }, []);
   return (
     <div>
-      <Widget
-        id={form_id}
-        height={height}
-        width={width}
-        className="my-form"
-        onSubmit={handleSubmit}
-      />
+      {loading ? (
+        <Loading />
+      ) : (
+        <Widget
+          id={form_id}
+          height={height}
+          width={width}
+          className="my-form"
+          onSubmit={handleSubmit}
+        />
+      )}
     </div>
   );
 }
