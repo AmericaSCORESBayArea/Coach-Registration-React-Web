5a5e03062bc208f72efc45566b6bb0d05ff5c6dc
Lautaro Rozenblum <lautirozen@gmail.com>
2023-04-26 19:50:09 -0300

    added loading animation

---
+++ src/components/utils/Loading.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Box from "@mui/material/Box";
+
+export default function Loading(props) {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <img
+        style={{
+          width: "300px",
+          justifyContent: "center",
+          verticalAlign: "middle",
+          margin: "auto",
+          alignSelf: "center",
+        }}
+        src={require("../../assets/Scores_Logo.gif")}
+        alt=""
+      />
+    </div>
+  );
+}
