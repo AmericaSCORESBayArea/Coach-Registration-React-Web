2dc7d5f03eb6d7cfc10b0642e2a11312cd76b6aa
Lautaro Rozenblum <lautirozen@gmail.com>
2023-05-29 16:24:47 -0300

    added new arrays

---
+++ src/components/utils/Arrays.js
@@ -0,0 +1,427 @@
+export const regionsArray = [
+  {
+    label: "Hayward",
+    value: "Hayward",
+  },
+  {
+    label: "Oakland",
+    value: "Oakland",
+  },
+  {
+    label: "San Francisco Civic Center",
+    value: "San Francisco Civic Center",
+  },
+  {
+    label: "San Francisco Crocker",
+    value: "San Francisco Crocker",
+  },
+  {
+    label: "San Rafael",
+    value: "San Rafael",
+  },
+];
+
+export const schoolsName = {
+  Hayward: {
+    schools: [
+      {
+        label: "Burbank Elementary School",
+        value: "Burbank Elementary School",
+      },
+      {
+        label: "Cherryland Elementary School",
+        value: "Cherryland Elementary School",
+      },
+      {
+        label: "East Avenue Elementary School",
+        value: "East Avenue Elementary School",
+      },
+      {
+        label: "Park Elementary School",
+        value: "Park Elementary School",
+      },
+      {
+        label: "Stonebrae Elementary School",
+        value: "Stonebrae Elementary School",
+      },
+      {
+        label: "Strobridge Elementary School",
+        value: "Strobridge Elementary School",
+      },
+      {
+        label: "Cesar Chavez Middle School",
+        value: "Cesar Chavez Middle School",
+      },
+      {
+        label: "MLK Middle School Hayward",
+        value: "MLK Middle School Hayward",
+      },
+      {
+        label: "Winton Middle School",
+        value: "Winton Middle School",
+      },
+      {
+        label: "Ochoa Middle School",
+        value: "Ochoa Middle School",
+      },
+      {
+        label: "Bret Harte Hayward Middle School",
+        value: "Bret Harte Hayward Middle School",
+      },
+      {
+        label: "Longwood Elementary School Hayward",
+        value: "Longwood Elementary School Hayward",
+      },
+      {
+        label: "Palma Ceia Elementary School",
+        value: "Palma Ceia Elementary School",
+      },
+      {
+        label: "Glassbrook Elementary School",
+        value: "Glassbrook Elementary School",
+      },
+      {
+        label: "Harder Elementary School",
+        value: "Harder Elementary School",
+      },
+      {
+        label: "Tyrrell Elementary School",
+        value: "Tyrrell Elementary School",
+      },
+      {
+        label: "Fairview Elementary School",
+        value: "Fairview Elementary School",
+      },
+      {
+        label: "Treeview Elementary School",
+        value: "Treeview Elementary School",
+      },
+      {
+        label: "Schafer Elementary School",
+        value: "Schafer Elementary School",
+      },
+      {
+        label: "Bowman Elementary School",
+        value: "Bowman Elementary School",
+      },
+      {
+        label: "Eldridge Elementary School",
+        value: "Eldridge Elementary School",
+      },
+      {
+        label: "Faith Ringgold Elementary School",
+        value: "Faith Ringgold Elementary School",
+      },
+      {
+        label: "Ruus Elementary School",
+        value: "Ruus Elementary School",
+      },
+      {
+        label: "Southgate Elementary School",
+        value: "Southgate Elementary School",
+      },
+      {
+        label: "Lorin Eden Elementary School",
+        value: "Lorin Eden Elementary School",
+      },
+      {
+        label: "Eden Gardens Elementary School",
+        value: "Eden Gardens Elementary School",
+      },
+    ],
+  },
+  Oakland: {
+    schools: [
+      {
+        label: "ASCEND Elementary School",
+        value: "ASCEND Elementary School",
+      },
+      {
+        label: "Coliseum Collegiate Prep Academy",
+        value: "Coliseum Collegiate Prep Academy",
+      },
+      {
+        label: "Montera Middle School",
+        value: "Montera Middle School",
+      },
+      {
+        label: "Westlake Middle School",
+        value: "Westlake Middle School",
+      },
+      {
+        label: "Esperanza Elementary School",
+        value: "Esperanza Elementary School",
+      },
+      {
+        label: "Korematsu Discovery Academy",
+        value: "Korematsu Discovery Academy",
+      },
+      {
+        label: "Manzanita Community School & Manzanita SEED",
+        value: "Manzanita Community School & Manzanita SEED",
+      },
+      {
+        label: "Elmhurst Middle School",
+        value: "Elmhurst Middle School",
+      },
+      {
+        label: "Global Family Elementary School",
+        value: "Global Family Elementary School",
+      },
+      {
+        label: "Howard Elementary School",
+        value: "Howard Elementary School",
+      },
+      {
+        label: "Sankofa Academy",
+        value: "Sankofa Academy",
+      },
+      {
+        label: "Glenview Elementary School",
+        value: "Glenview Elementary School",
+      },
+      {
+        label: "Prescott Elementary School",
+        value: "Prescott Elementary School",
+      },
+      {
+        label: "Greenleaf Elementary School",
+        value: "Greenleaf Elementary School",
+      },
+      {
+        label: "EnCompass Elementary School",
+        value: "EnCompass Elementary School",
+      },
+      {
+        label: "Bridges Academy Elementary School",
+        value: "Bridges Academy Elementary School",
+      },
+      {
+        label: "MLK Lafayette Elementary School",
+        value: "MLK Lafayette Elementary School",
+      },
+      {
+        label: "International Community School & Think College Now",
+        value: "International Community School & Think College Now",
+      },
+      {
+        label: "Learning Without Limits Elementary School",
+        value: "Learning Without Limits Elementary School",
+      },
+      {
+        label: "Bret Harte Oakland Middle School",
+        value: "Bret Harte Oakland Middle School",
+      },
+      {
+        label: "Madison Park Primary",
+        value: "Madison Park Primary",
+      },
+      {
+        label: "Futures Elementary School",
+        value: "Futures Elementary School",
+      },
+      {
+        label: "Futures & Community United Elementary School",
+        value: "Futures & Community United Elementary School",
+      },
+      {
+        label: "Life Academy",
+        value: "Life Academy",
+      },
+    ],
+  },
+  "San Francisco Civic Center": {
+    schools: [
+      {
+        label: "Bessie Carmichael",
+        value: "Bessie Carmichael",
+      },
+      {
+        label: "Redding Elementary School",
+        value: "Redding Elementary School",
+      },
+      {
+        label: "Up on Top - Program Site",
+        value: "Up on Top - Program Site",
+      },
+      {
+        label: "Salvation Army Kroc Center - Program Site",
+        value: "Salvation Army Kroc Center - Program Site",
+      },
+      {
+        label: "John Muir Elementary School",
+        value: "John Muir Elementary School",
+      },
+      {
+        label: "Indochinese Housing Development Corporation - Lead Agency",
+        value: "Indochinese Housing Development Corporation - Lead Agency",
+      },
+    ],
+  },
+  "San Francisco Crocker": {
+    schools: [
+      {
+        label: "Francisco Middle School",
+        value: "Francisco Middle School",
+      },
+      {
+        label: "TECA Middle School - Program Site",
+        value: "TECA Middle School - Program Site",
+      },
+      {
+        label: "Visitation Valley Middle School",
+        value: "Visitation Valley Middle School",
+      },
+      {
+        label: "Everett Middle School",
+        value: "Everett Middle School",
+      },
+      {
+        label: "MLK Academic Middle School SF",
+        value: "MLK Academic Middle School SF",
+      },
+      {
+        label: "TECA Elementary School - Program Site",
+        value: "TECA Elementary School - Program Site",
+      },
+      {
+        label: "Bryant Elementary School",
+        value: "Bryant Elementary School",
+      },
+      {
+        label: "Sanchez Elementary School",
+        value: "Sanchez Elementary School",
+      },
+      {
+        label: "Marshall Elementary School",
+        value: "Marshall Elementary School",
+      },
+      {
+        label: "Flynn Elementary School",
+        value: "Flynn Elementary School",
+      },
+      {
+        label: "Hillcrest Elementary School",
+        value: "Hillcrest Elementary School",
+      },
+      {
+        label: "Bret Harte Elementary School",
+        value: "Bret Harte Elementary School",
+      },
+      {
+        label: "ER Taylor Elementary School",
+        value: "ER Taylor Elementary School",
+      },
+      {
+        label: "Paul Revere Elementary School",
+        value: "Paul Revere Elementary School",
+      },
+      {
+        label: "Cleveland Elementary School",
+        value: "Cleveland Elementary School",
+      },
+      {
+        label: "Junipero Serra Elementary",
+        value: "Junipero Serra Elementary",
+      },
+      {
+        label: "Paul Revere Middle School",
+        value: "Paul Revere Middle School",
+      },
+      {
+        label: "Willie Brown Middle School",
+        value: "Willie Brown Middle School",
+      },
+      {
+        label: "Joseph Lee Rec Center Bayview",
+        value: "Joseph Lee Rec Center Bayview",
+      },
+    ],
+  },
+  "San Rafael": {
+    schools: [
+      {
+        label: "Bahia Vista Elementary School",
+        value: "Bahia Vista Elementary School",
+      },
+      {
+        label: "Davidson Middle School",
+        value: "Davidson Middle School",
+      },
+      {
+        label: "Laurel Dell Elementary School",
+        value: "Laurel Dell Elementary School",
+      },
+      {
+        label: "San Pedro Elementary School",
+        value: "San Pedro Elementary School",
+      },
+      {
+        label: "Venetia Valley K-8 School",
+        value: "Venetia Valley K-8 School",
+      },
+      {
+        label: "Coleman Elementary School",
+        value: "Coleman Elementary School",
+      },
+    ],
+  },
+};
+
+export const genderArray = [
+  {
+    label: "Female",
+    value: "Female",
+  },
+  {
+    label: "Male",
+    value: "Male",
+  },
+  {
+    label: "Non-binary",
+    value: "Non-binary",
+  },
+  {
+    label: "Prefer not to say",
+    value: "Prefer not to say",
+  },
+];
+
+export const ethnicityArray = [
+  {
+    label: "African American",
+    value: "African American",
+  },
+  {
+    label: "Asian",
+    value: "Asian",
+  },
+  {
+    label: "Caucasian",
+    value: "Caucasian",
+  },
+  {
+    label: "Filipino",
+    value: "Filipino",
+  },
+  {
+    label: "Hispanic/Latinx",
+    value: "Hispanic/Latinx",
+  },
+  {
+    label: "Middle Eastern/Arabic",
+    value: "Middle Eastern/Arabic",
+  },
+  {
+    label: "Multi-Racial/Multi-Ethnic",
+    value: "Multi-Racial/Multi-Ethnic",
+  },
+  {
+    label: "Native American",
+    value: "Native American",
+  },
+  {
+    label: "Pacific Islander",
+    value: "Pacific Islander",
+  },
+];
